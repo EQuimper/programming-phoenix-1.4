0f2dbfc0b60b1132d9935748185e310cd19aeacd
Emanuel Quimper <quimperemanuel@gmail.com>
2018-06-02 11:35:16 -0400

    Add hardcoded users

---
+++ lib/rumbl/accounts/accounts.ex
@@ -0,0 +1,26 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  alias Rumbl.Accounts.User
+
+  def list_users do
+    [
+      %User{id: "1", name: "Jose", username: "josevalim"},
+      %User{id: "2", name: "Bruce", username: "redrapid"},
+      %User{id: "3", name: "Chris", username: "chrismccord"}
+    ]
+  end
+
+  def get_user(id) do
+    Enum.find(list_users(), fn map -> map.id == id end)
+  end
+
+  def get_user_by(params) do
+    Enum.find(list_users(), fn map ->
+      Enum.all?(params, fn {key, val} ->
+        Map.get(map, key) == val end)
+    end)
+  end
+end
